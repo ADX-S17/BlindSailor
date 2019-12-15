d3e0a59f08fb7b57b991ed381917105d52cb9642
adx <antony.desmeaux@gmail.com>
2019-12-15 23:10:48 +0100

    Upload of Frederico's job on GPS

---
+++ BlindSailor_v0.1/GNSS_FedericoCislaghi/PlotGPS_Raspberry.py
@@ -0,0 +1,100 @@
+# -*- coding: utf-8 -*-
+
+"""PlotGPS.py: Récupère et affiche les informations sur les satellites  GPS en visibilité."""
+
+__author__   = "Federico CISLAGHI"
+__email__    = "cislaghi47@gmail.com"
+
+import serial
+import pynmea2
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib
+
+## Script configuration
+# port = "COM8"
+port = '/dev/ttyAMA0' 
+
+## Data initialisation
+GSV_Data_Complete = False
+NbMsg = 0
+SV_PRN_num = [0] * 20
+SV_azimuth = [0] * 20
+SV_elevation = [0] * 20
+SV_SNR = [0] * 20
+
+print("Ouverture du port série.")
+serialPort = serial.Serial(port, baudrate = 9600, timeout = 0.5)
+
+while not(GSV_Data_Complete):
+    str = serialPort.readline().decode('ascii', errors='replace').strip()
+
+    try:
+        msg = pynmea2.parse(str)
+    except:
+        continue
+    
+    if msg.sentence_type == 'GSV':
+        print(str)
+        Nb_SV_In_View = int(msg.num_sv_in_view)
+        Num_CurMsg = int(msg.msg_num)
+
+        offset = (Num_CurMsg - 1) * 4
+        # Enregistrement des données dans les tableaux
+        SV_PRN_num[offset + 0] = int(msg.sv_prn_num_1)
+        SV_azimuth[offset + 0] = int(msg.azimuth_1)
+        SV_elevation[offset + 0] = int(msg.elevation_deg_1)
+        SV_SNR[offset + 0] = int(msg.snr_1)
+        if offset + 2 <= Nb_SV_In_View:
+            SV_PRN_num[offset + 1] = int(msg.sv_prn_num_2)
+            SV_azimuth[offset + 1] = int(msg.azimuth_2)
+            SV_elevation[offset + 1] = int(msg.elevation_deg_2)
+            SV_SNR[offset + 1] = int(msg.snr_2)
+        if offset + 3 <= Nb_SV_In_View:
+            SV_PRN_num[offset + 2] = int(msg.sv_prn_num_3)
+            SV_azimuth[offset + 2] = int(msg.azimuth_3)
+            SV_elevation[offset + 2] = int(msg.elevation_deg_3)
+            SV_SNR[offset + 2] = int(msg.snr_3)
+        if offset + 4 <= Nb_SV_In_View:
+            SV_PRN_num[offset + 3] = int(msg.sv_prn_num_4)
+            SV_azimuth[offset + 3] = int(msg.azimuth_4)
+            SV_elevation[offset + 3] = int(msg.elevation_deg_4)
+            SV_SNR[offset + 3] = int(msg.snr_4)
+
+        # On attend le message 1/N pour configurer le nb de messages à recevoir
+        if Num_CurMsg == 1:
+            NbMsg = int(msg.num_messages)
+            MsgReceived = [False] * NbMsg
+            
+        if NbMsg > 0:
+            MsgReceived[Num_CurMsg - 1] = True
+            
+            if all(MsgReceived):
+                GSV_Data_Complete = True
+        
+
+SV_PRN_num = SV_PRN_num[0:Nb_SV_In_View]
+SV_azimuth_deg = SV_azimuth[0:Nb_SV_In_View]
+SV_elevation = SV_elevation[0:Nb_SV_In_View]
+SV_SNR = SV_SNR[0:Nb_SV_In_View]
+
+SV_azimuth_rad = [x/180.0*3.141593 for x in SV_azimuth_deg]
+
+print("Tracé de la figure.") 
+## Tracé de la figure
+fig = plt.figure()
+ax = fig.add_subplot(111, polar=True)
+ax.set_theta_zero_location('N')
+ax.set_theta_direction(-1)
+ax.set_rlim(0, 90)
+c = ax.scatter(SV_azimuth_rad, SV_elevation, c=SV_SNR, s=350,
+               cmap='rainbow', alpha=0.75, vmin=0)
+
+for i, txt in enumerate(SV_PRN_num):
+    ax.annotate(txt, (SV_azimuth_rad[i], SV_elevation[i]))
+
+cbar = fig.colorbar(c)
+ax.set_title("%d GPS satellites in view" % Nb_SV_In_View, fontsize=18)
+plt.show()
+
+print('Done!')
