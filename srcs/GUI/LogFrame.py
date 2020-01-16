2341d0a165bfc20f7b1edca2da84a1748c6c9d17
mdufaud <mdufaud@student.42.fr>
2020-01-16 12:48:16 +0100

    transfert

--- srcs/GUI/LogFrame.py
+++ srcs/GUI/LogFrame.py
@@ -2,11 +2,11 @@
 #coding: utf-8
 
 """ System """
-from __future__ import print_function
-
 import os
 import sys
 import logging
+
+from sihd.srcs import Core
 
 try:
     import wx
@@ -15,27 +15,47 @@ except ImportError:
     wx = None
     Panel = object
 
+class CursesHandler(logging.Handler):
+
+    def __init__(self, panel):
+        logging.Handler.__init__(self)
+        self.panel = panel
+
+    def emit(self, record):
+        msg = self.format(record)
+        panel = self.panel
+        panel.log(msg)
+
 class LogFrame(Panel):
 
     def __init__(self, *args, **kwargs):
         super(LogFrame, self).__init__(*args, **kwargs)
-        self.createControls()
-        self.bindEvents()
-        self.doLayout()
+        self.create_controls()
+        self.bind_events()
+        self.do_layout()
 
-    def createControls(self):
+    def add_logger(self):
+        log_handler = CursesHandler(win)
+        log_handler.setFormatter(Core.ILoggable.get_formatter())
+        Core.ILoggable.logger.addHandler(log_handler)
+        self._log_handler = log_handler
+
+    def remove_logger(self):
+        Core.ILoggable.logger.removeHandler(self._log_handler)
+
+    def create_controls(self):
         self.logger = wx.TextCtrl(self,
                 size=(800, 400),
                 #style=wx.TE_MULTILINE | wx.TE_READONLY)
                 style=wx.TE_MULTILINE)
 
-    def bindEvents(self):
-        self.logger.Bind(wx.EVT_TEXT, self.onTextEntered)
+    def bind_events(self):
+        self.logger.Bind(wx.EVT_TEXT, self.on_text_entered)
 
-    def onTextEntered(self, event):
+    def on_text_entered(self, event):
         text = event.GetString()
 
-    def doLayout(self):
+    def do_layout(self):
         boxSizer = wx.BoxSizer(orient=wx.HORIZONTAL)
         boxSizer.Add(self.logger)
         self.SetSizerAndFit(boxSizer)
